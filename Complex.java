bf19a8942b3c51cafad26bfea8512bbdb5db9539
Raghav Maheshwari <34077214+raghavddps2@users.noreply.github.com>
2018-10-27 19:54:54 +0530

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -1,15 +1,18 @@
 import java.util.*;
 public class Complex{
+    Scanner sc = new Scanner(System.in);
     int a;
     int b;
 
-    Complex(int a,int b){
-        this.a = a;
-        this.b = b;
-    }
     Complex(){
         this.a = 0;
         this.b = 0;
+    }
+    public void get(){
+        System.out.print("Enter the real part:   ");
+        a = sc.nextInt();
+        System.out.print("Ener the imaginary part:   ");
+        b = sc.nextInt();
     }
     public Complex sum_c(Complex c1, Complex c2){
         Complex c3 = new Complex();
@@ -36,32 +39,62 @@ public class Complex{
         Scanner sc = new Scanner(System.in);
         Complex c1 = new Complex();
         Complex c2 = new Complex();
-        System.out.println("Enter the Complex Number 1");
-        System.out.print("Enter the real part:   ");
-        c1.a = sc.nextInt();
-        System.out.print("Ener the imaginary part:   ");
-        c1.b = sc.nextInt();
-        System.out.println("------------------------------------------------");
-        System.out.println("Enter the Complex Number 2");
-        System.out.print("Enter the real part:   ");
-        c2.a = sc.nextInt();
-        System.out.print("Ener the imaginary part:   ");
-        c2.b = sc.nextInt();
-
-        System.out.println("------------------------------------------------");
         Complex compute = new Complex();
-        compute = compute.sum_c(c1,c2);
-        System.out.print("The sum of two complex numbers is: ");
-        System.out.println(compute.a + " + "+ compute.b + " i");
         
-        System.out.println("------------------------------------------------");
-        compute = compute.diff_c(c1,c2);
-        System.out.print("The difference of two complex numbers is: ");
-        System.out.println(compute.a + " + "+ compute.b + " i");
-        
-        System.out.println("------------------------------------------------");
-        compute = compute.mul_c(c1,c2);
-        System.out.print("The multiplication of two complex numbers is: ");
-        System.out.println(compute.a +" + "+"("+ (-1*compute.b) +")"+ " i");
+
+        while(true){
+            System.out.println("**********************Menu*******************************************");
+            System.out.println("1.Add two complex Numbers");
+            System.out.println("2.Subtract two complex Numbers");
+            System.out.println("3.Multiply two complex Numbers");
+            System.out.println("4. EXIT");
+
+            int choice;
+            System.out.println("Enter your choice");
+            choice = sc.nextInt();
+
+            switch(choice){
+                case 1:
+                        System.out.println("Enter the Complex Number 1");
+                        c1.get();
+                        System.out.println("------------------------------------------------");
+                        System.out.println("Enter the Complex Number 2");
+                        c2.get();
+                        System.out.println("------------------------------------------------");
+                        compute = compute.sum_c(c1,c2);
+                        System.out.print("The sum of two complex numbers is: ");
+                        System.out.println(compute.a + " + "+ compute.b + " i");
+                        break;
+                
+                case 2:
+                        System.out.println("Enter the Complex Number 1");
+                        c1.get();
+                        System.out.println("------------------------------------------------");
+                        System.out.println("Enter the Complex Number 2");
+                        c2.get();
+                        System.out.println("------------------------------------------------");
+                        compute = compute.diff_c(c1,c2);
+                        System.out.print("The difference of two complex numbers is: ");
+                        System.out.println(compute.a + " + "+ compute.b + " i");
+                        break;
+                
+                case 3:
+                        System.out.println("Enter the Complex Number 1");
+                        c1.get();
+                        System.out.println("------------------------------------------------");
+                        System.out.println("Enter the Complex Number 2");
+                        c2.get();
+                        System.out.println("------------------------------------------------");
+                        compute = compute.mul_c(c1,c2);
+                        System.out.print("The multiplication of two complex numbers is: ");
+                        System.out.println(compute.a +" + "+"("+ (-1*compute.b) +")"+ " i");
+                        break;
+
+                case 4:
+                        System.exit(0);
+                default:
+                        System.out.println("INVALID CHOICE");
+            }
+        }
     }
 }
