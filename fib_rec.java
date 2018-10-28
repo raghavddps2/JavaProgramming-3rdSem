90ec91777392a0192ad98bd0bf5f8e8b770d2bef
Raghav Maheshwari <34077214+raghavddps2@users.noreply.github.com>
2018-10-28 18:46:50 +0530

    Create fib_rec.java

---
+++ fib_rec.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class fib_rec{
+    public static int fib(int n){
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return (fib(n-1)+fib(n-2));
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no of terms you want");
+        int n = sc.nextInt();
+
+        System.out.print("The fibonacci series is:   ");
+        for(int i=0;i<n;i++){
+            System.out.print(fib(i)+"  ");
+        }
+
+    }
+}
